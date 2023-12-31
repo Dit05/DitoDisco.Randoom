8481b7f87769fd1ca1cd4acdb20488666641ae6b
Kiki <mullerkrisztianbusiness@gmail.com>
2023-12-31 01:33:57 +0100

    Added generator speed test.

--- DitoDisco.Randoom.Tests/GeneratorTests.cs
+++ DitoDisco.Randoom.Tests/GeneratorTests.cs
@@ -5,21 +5,37 @@ namespace DitoDisco.Randoom.Tests;
 public class GeneratorTests {
 
     private readonly BitGenerator gen;
+    private readonly string idTag;
 
 
     public GeneratorTests(Type type) {
         gen = (BitGenerator)Activator.CreateInstance(type)!;
+        idTag = $"[{gen.GetType().Name}]";
     }
 
     [Test]
     public void BitSample() {
-        TestContext.WriteLine($"[{gen.GetType().Name}] Bit sample:");
+        TestContext.WriteLine($"{idTag} Bit sample:");
         for(int i = 0; i < 24; i++) {
             for(int j = 0; j < 60 /* 60 has a lot of divisors, which should help patterns stand out. */; j++) {
                 TestContext.Write(gen.NextBit() ? '1' : '0');
             }
             TestContext.WriteLine();
         }
+    }
+
+    [Test]
+    public void BitSpeedTest() {
+        System.Diagnostics.Stopwatch stopper = new System.Diagnostics.Stopwatch();
+
+        const int COUNT = 100_000;
+
+        stopper.Start();
+        for(int i=0;i<COUNT;i++)gen.NextBit();//Make sure to keep this benchmarked part compact so that it runs faster !!
+        stopper.Stop();
+
+
+        TestContext.WriteLine($"{idTag} Generated {COUNT} bits in {stopper.Elapsed.TotalMilliseconds} ms.");
     }
 
 }
